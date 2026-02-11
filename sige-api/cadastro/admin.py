44ee74e7929b11dcc5feef217ad3ba58d20da6b3
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-11 16:54:07 -0500

    feat: colocando a classe ItemGenerico e adicionando as classe Endereco, Fornecedor e ItemGenerico para a visualização de administrador

--- sige-api/cadastro/admin.py
+++ sige-api/cadastro/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from cadastro.models import Endereco, Fornecedor, ItemGenerico
+
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ("id", "lagradouro", "numero", "bairro", "cep", "municipio", "estado")
+    list_display_links = ("id", "lagradouro")
+    search_fields = ("id",)
+    
+admin.site.register(Endereco, EnderecoAdmin)
+    
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ("id", "razao_social", "nome_fantasia", "cnpj", "telefone", "email", "endereco")
+    list_display_links = ("id", "razao_social", "cnpj")
+    search_fields = ("id", "razao_social", "cnpj")
+
+admin.site.register(Fornecedor, FornecedorAdmin)
+
+class ItemGenericoAdmin(admin.ModelAdmin):
+    list_display = ("id", "catmat", "descricao", "unidade_medida", "categoria")
+    list_display_links = ("id", "catmat", "descricao")
+    search_fields = ("id", "catmat", "descricao")
+    
+admin.site.register(ItemGenerico, ItemGenericoAdmin)
+
 
 # Register your models here.
--- sige-api/cadastro/models.py
+++ sige-api/cadastro/models.py
@@ -11,14 +11,41 @@ class Endereco(models.Model):
     
     def __str__(self):
         return f"{self.lagradouro}, {self.numero} - {self.municipio}/{self.estado}"
+    
 class Fornecedor(models.Model):
     razao_social = models.CharField(max_length=255)
     nome_fantasia = models.CharField(max_length=255)
-    cnpj = models.CharField(max_length=18,unique=True)
+    cnpj = models.CharField(max_length=18,unique=True, blank=False, null=False, verbose_name="CNPJ")
     telefone = models.CharField(max_length=20,blank=True,null=True)
     email = models.EmailField(max_length=255,blank=True,null=True)
     endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT,related_name='fornecedores')
 
-
-
-
+class ItemGenerico(models.Model):
+    catmat = models.CharField(max_length = 6, unique=True, blank=False, null=False)
+    descricao = models.CharField(max_length=300)
+    unidades_de_medida = (
+        ("KG", "Quilograma (KG)"),
+        ("G", "Grama (G)"),
+        ("L", "Litro (L)"),
+        ("mL", "Mililitro (mL)"),
+        ("duzia", "Duzia"),
+        ("cento", "Cento"),
+        ("PCT", "Pacote (PCT)"),
+        ("CX", "Caixa (CX)"),
+        ("FND", "Fardo (FND)"),
+        ("GAR", "Garrafa (GAR)"),
+        ("lata", "Lata")
+    )
+    unidade_medida = models.CharField(max_length=5, choices=unidades_de_medida, default="KG", blank=False, null=False)
+    
+    categorias_de_alimento = (
+        ("tempS", "Tempero Secos"),
+        ("SM", "Secos / Mercearia"),
+        ("Lac", "Lácteos e Derivados"),
+        ("Oli", "Óleos, Azeites e Vinagres"), 
+        ("MolCo", "Molhos e Condimentos"),
+        ("Fr","Frutas"), 
+        ("Le","Legumes")
+    )
+    categoria = models.CharField(max_length=5, choices=categorias_de_alimento, blank=False, null=False)
+    
