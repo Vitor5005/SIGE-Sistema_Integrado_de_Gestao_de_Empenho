16462141bcab83636ce2eb90e5c6f343f78ad093
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-11 16:51:38 -0500

    feat: Colocando cada uma das "apps" no INSTALLED_APPS

--- sige-api/sige_api/settings.py
+++ sige-api/sige_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cadastro',
+    'empenho',
+    "entrega",
+    "licitacao",
+    "rest_framework"
+    
 ]
 
 MIDDLEWARE = [
