63ec81e8338a6e96ccc342ae0262e09d8cb5c84d
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-11 18:16:25 -0500

    feat: adicionando as classes OrdemEntrega e ItemOrdem no app_entrega e permitindo a suas visualizações na pagina de administração

--- sige-api/entrega/admin.py
+++ sige-api/entrega/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from entrega.models import OrdemEntrega, ItemOrdem
+
+class OrdemEntregaAdmin(admin.ModelAdmin):
+    list_display = ("id", "codigo", "empenho", "status", "data_emissao", "data_entrega", "valor_total_executado")
+    list_display_links = ("id", "codigo")
+    search_fields = ("id", "codigo")
+    
+admin.site.register(OrdemEntrega, OrdemEntregaAdmin)
+
+class ItemOrdemAdmin(admin.ModelAdmin):
+    list_display = ("id", "ordem_entrega", "item_empenho", "quantidade_solicitada", "quantidade_entregue", "observacao")
+    list_display_links = ("id",)
+    search_fields = ("id",)
+
+admin.site.register(ItemOrdem, ItemOrdemAdmin)
 
 # Register your models here.
--- sige-api/entrega/models.py
+++ sige-api/entrega/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from empenho.models import Empenho, ItemEmpenho
 
+class OrdemEntrega(models.Model):
+    
+    empenho = models.ForeignKey(Empenho, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=20, unique=True, null=False, blank=False)
+    status_tipo = (
+        ("esp", "Em espera"),
+        ("con", "Concluída")
+    )
+    status = models.CharField(max_length=3, choices=status_tipo, default="esp")
+    data_emissao = models.DateField(auto_now_add=True)
+    data_entrega = models.DateField(null=True, blank=True)
+    valor_total_executado = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.codigo
+
+class ItemOrdem(models.Model):
+    ordem_entrega = models.ForeignKey(OrdemEntrega, on_delete=models.CASCADE)
+    item_empenho = models.ForeignKey(ItemEmpenho, on_delete=models.CASCADE)
+    quantidade_solicitada = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade_entregue = models.DecimalField(max_digits=10, decimal_places=2)
+    observacao = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ordem_entrega.codigo} - {self.item_empenho.codigo}"
+
+    
 # Create your models here.
