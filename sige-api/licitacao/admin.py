2b72acb2f4839651b0aa1cec7c67a00239575dbc
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-11 17:52:51 -0500

    Feat: adicionando as classes Licitacao, Ata e ItemAta e adicionando suas visualizações a pagina de administrador

--- sige-api/licitacao/admin.py
+++ sige-api/licitacao/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from licitacao.models import Licitacao, Ata, ItemAta
 
-# Register your models here.
+class LicitacaoAdmin(admin.ModelAdmin):
+    list_display = ("id", "numero_licitacao", "validade", "data_abertura")
+    list_display_links =  ("id", "numero_licitacao")
+    search_fields = ("id", "numero_licitacao")
+    
+admin.site.register(Licitacao, LicitacaoAdmin)
+
+class AtaAdmin(admin.ModelAdmin):
+    list_display = ("id", "numero_ata", "ata_saldo_total", "licitacao", "fornecedor")
+    list_display_links = ("id", "numero_ata")
+    search_fields = ("id", "numero_ata")
+    
+admin.site.register(Ata, AtaAdmin)
+
+class ItemAtaAdmin(admin.ModelAdmin):
+    list_display = ("id", "ata", "item_generico", "marca", "quantidade_licitada", "valor_unitario")
+    list_display_links = ("id", "marca")
+    search_fields = ("id", "marca")
+    
+admin.site.register(ItemAta, ItemAtaAdmin)
--- sige-api/licitacao/models.py
+++ sige-api/licitacao/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from cadastro.models import Fornecedor, ItemGenerico
+
+class Licitacao(models.Model):
+    numero_licitacao = models.CharField(max_length=50, unique=True, blank=False, null=False, verbose_name="Número da Licitação")
+    validade = models.IntegerField(blank=False, null=False, verbose_name="Validade (em meses)")
+    data_abertura = models.DateField(blank=False, null=False, verbose_name="Data de Abertura")
+
+    def __str__(self):
+        return f"Licitacao {self.numero_licitacao} \n Validade: {self.validade} meses \n Data de Abertura: {self.data_abertura}"
+
+
+class Ata(models.Model):
+    numero_ata = models.CharField(max_length=50, unique=True, blank=False, null=False, verbose_name="Número da Ata")
+    ata_saldo_total = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, verbose_name="Saldo Total da Ata")
+    licitacao = models.ForeignKey(Licitacao, on_delete=models.CASCADE)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('licitacao', 'fornecedor')
+    
+    def __str__(self):
+        return f"Ata {self.numero_ata} \n Saldo Total: {self.ata_saldo_total} \n Licitação: {self.licitacao.numero_licitacao} \n Fornecedor: {self.fornecedor.razao_social}"
+
+class ItemAta(models.Model):
+    ata = models.ForeignKey(Ata, on_delete=models.CASCADE)
+    item_generico = models.ForeignKey(ItemGenerico, on_delete=models.CASCADE)
+    marca = models.CharField(max_length=255, blank=False, null=False, verbose_name="Marca do Item")
+    quantidade_licitada = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, verbose_name="Quantidade Licitada")
+    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, verbose_name="Valor Unitário")
+    
+    def __str__(self):
+        return f"item: {self.item_generico.descricao} \n Marca: {self.marca} \n Quantidade Licitada: {self.quantidade_licitada} \n Valor Unitário: {self.valor_unitario} \n Ata: {self.ata.numero_ata}"
 
 # Create your models here.
