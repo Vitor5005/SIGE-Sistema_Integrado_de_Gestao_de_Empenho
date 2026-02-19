6f5f11b228cbc2ec8b57d5638b31d484ae0cd795
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-19 08:28:50 -0500

    Update: Arrumando funcionamento do docker

--- sige-api/entrypoint.sh
+++ sige-api/entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-echo "Esperando o banco de dados..."
-
-while ! nc -z db 3306; do
-  sleep 1
-done
-
-echo "Banco iniciado!"
-
+echo "Banco de dados pronto! Iniciando as migrações..."
 python manage.py migrate
 
 echo "Criando superusuário se não existir..."
-
 python manage.py createsuperuser --noinput || true
 
-echo "Iniciando servidor..."
-
+echo "Iniciando o servidor da API..."
 python manage.py runserver 0.0.0.0:8000
