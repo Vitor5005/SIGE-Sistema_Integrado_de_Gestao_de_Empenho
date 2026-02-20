2f430b1a683de7a9ee169444245099007918d478
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-20 16:16:50 -0500

    feat: populando o banco

--- sige-api/entrypoint.sh
+++ sige-api/entrypoint.sh
@@ -12,5 +12,8 @@ python manage.py migrate
 echo "Criando superusuário se não existir..."
 python manage.py createsuperuser --noinput || true
 
+echo "Populando dados iniciais..."
+python seed.py
+
 echo "Iniciando o servidor da API..."
 python manage.py runserver 0.0.0.0:8000
