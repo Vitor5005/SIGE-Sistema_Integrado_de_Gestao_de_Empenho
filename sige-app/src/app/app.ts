1b335f1d88d198f17bf0b743284ebe5dd38f8e6a
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-23 09:24:04 -0500

    update: removendo imports não utilizados no app.ts

--- sige-app/src/app/app.ts
+++ sige-app/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from './component/utils/cabecalho/cabecalho';
-import { AdicionarLicitacao } from './component/adicionar-licitacao/adicionar-licitacao';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, AdicionarLicitacao],
+  imports: [RouterOutlet, Cabecalho],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
