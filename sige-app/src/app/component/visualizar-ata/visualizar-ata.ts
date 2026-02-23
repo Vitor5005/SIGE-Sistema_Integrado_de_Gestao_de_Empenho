370fb46700626096d2735cadfe69a3bd4d5bb855
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-23 15:43:08 -0500

    feat: Enviando a pagina de visualização de uma ata especifica

--- sige-app/src/app/component/visualizar-ata/visualizar-ata.ts
+++ sige-app/src/app/component/visualizar-ata/visualizar-ata.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-visualizar-ata',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './visualizar-ata.html',
   styleUrl: './visualizar-ata.scss',
 })
 export class VisualizarAta {
+
+  constructor(private router: Router){}
+
+  enviarPara(rota: string){
+      this.router.navigate([rota]);
+  }
 
 }
