f9c9fe71bc1821d10f6b8be30f0c8862f7d9509b
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-24 13:46:09 -0500

    feat: enviando o modal da pagina  entragas, esse modal vai usado para confirmar as entregas

--- sige-app/src/app/component/visualizar-entregas/visualizar-entregas.ts
+++ sige-app/src/app/component/visualizar-entregas/visualizar-entregas.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BarraPesquisa } from '../utils/barra-pesquisa/barra-pesquisa';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visualizar-entregas',
@@ -10,6 +11,26 @@ import { CommonModule } from '@angular/common';
 })
 export class VisualizarEntregas {
 
-  confirmado : boolean = false;
+  confirmado: boolean = false;
+
+  constructor(private router: Router) { }
+
+  @ViewChild('myModal') modal!: ElementRef;
+  @ViewChild('myInput') input!: ElementRef;
+
+  enviarPara(rota: string) {
+    this.router.navigate([rota]);
+  }
+
+  ngAfterViewInit() {
+
+    const modalElement = this.modal.nativeElement;
+
+    modalElement.addEventListener('shown.bs.modal', () => {
+      this.input.nativeElement.focus();
+    });
+
+  }
+
 
 }
