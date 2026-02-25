2a655fa3d2b9c4fa9146170a6d3338daecc9c23f
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-25 11:06:05 -0500

    update: arrumando bug de mudar de pagina e o modal continuar aberto

--- sige-app/src/app/app.ts
+++ sige-app/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { Cabecalho } from './component/utils/cabecalho/cabecalho';
 
 @Component({
@@ -10,4 +10,29 @@ import { Cabecalho } from './component/utils/cabecalho/cabecalho';
 })
 export class App {
   protected readonly title = signal('sige-app');
+
+
+  constructor(private router: Router) {
+
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationStart) {
+        this.fecharModal();
+      }
+
+    });
+
+  }
+
+  fecharModal() {
+
+    document.body.classList.remove('modal-open');
+
+    const backdrops =
+      document.getElementsByClassName('modal-backdrop');
+
+    while (backdrops.length > 0) {
+      backdrops[0].remove();
+    }
+  }
 }
