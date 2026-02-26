c648c3382125e63869b9de1e76b14c4fa9f99da2
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-02-25 20:23:23 -0500

    feat: intergrando a funcionalidade de visualizar licitacoes

--- sige-app/src/app/component/visualizar-licitacao/visualizar-licitacao.ts
+++ sige-app/src/app/component/visualizar-licitacao/visualizar-licitacao.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { LicitacaoService } from '../../service/licitacao.service';
+import { Licitacao } from '../../model/licitacao';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-visualizar-licitacao',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './visualizar-licitacao.html',
   styleUrl: './visualizar-licitacao.scss',
 })
 export class VisualizarLicitacao {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private licitacaoService: LicitacaoService,
+  ) {}
+
+  licitacoes: Licitacao = {} as Licitacao; 
 
   enviarPara(rota: string) {
     this.router.navigate([rota]);
--- sige-app/src/app/component/visualizar-licitacoes/visualizar-licitacoes.ts
+++ sige-app/src/app/component/visualizar-licitacoes/visualizar-licitacoes.ts
@@ -1,18 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { BarraPesquisa } from '../utils/barra-pesquisa/barra-pesquisa';
 import { Router } from '@angular/router';
+import { Licitacao } from '../../model/licitacao';
+import { LicitacaoService } from '../../service/licitacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-licitacoes',
-  imports: [BarraPesquisa],
+  imports: [BarraPesquisa, CommonModule],
   templateUrl: './visualizar-licitacoes.html',
   styleUrl: './visualizar-licitacoes.scss',
 })
 export class VisualizarLicitacoes {
-  
-  constructor(private router: Router) {}
 
-  enviarPara(rota: string){
+  constructor(
+    private router: Router,
+    private licitacaoService: LicitacaoService,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    this.get();
+  }
+
+  registro: Licitacao[] = [];
+
+  get(): void {
+    this.licitacaoService.get().subscribe({
+      next: (resposta: Array<Licitacao>) => {
+        this.registro = resposta;
+        this.cdr.detectChanges();
+      }
+    });
+  };
+
+  ver(){
+    console.log(this.registro);
+  }
+
+  enviarPara(rota: string) {
     this.router.navigate([rota]);
   }
 
