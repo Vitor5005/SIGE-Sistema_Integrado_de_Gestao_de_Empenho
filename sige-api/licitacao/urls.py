97194e20a579ee951c22c732ab5c099d545a5481
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-03-02 12:24:26 -0500

    update: atualizando a rota do valor_empenho

--- sige-api/licitacao/urls.py
+++ sige-api/licitacao/urls.py
@@ -11,7 +11,7 @@ router = DefaultRouter()
 router.register(r'licitacoes',LicitacaoViewSet)
 router.register(r'atas', AtaViewSet)
 router.register(r'itematas', ItemAtaViewSet)
-router.register(r'ata/valor_empenho', ValorDoEmpenhoViewSet, basename='valor_empenho')
+router.register(r'atas/valor_empenho', ValorDoEmpenhoViewSet, basename='valor_empenho')
 
 
 urlpatterns = router.urls
