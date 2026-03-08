4edc64ffb369a753839b9391f0e60fc837000b30
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-03-08 04:55:04 -0500

    feat: intregrando a pagina de entregas (incompleto)

--- sige-app/src/app/component/visualizar-entregas/visualizar-entregas.ts
+++ sige-app/src/app/component/visualizar-entregas/visualizar-entregas.ts
@@ -2,6 +2,10 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BarraPesquisa } from '../utils/barra-pesquisa/barra-pesquisa';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { OrdemEntregaService } from '../../service/ordem-entrega.service';
+import { OrdemEntrega } from '../../model/ordem_entrega';
+import { ItemOrdemService } from '../../service/item-ordem.service';
+import { ItemOrdem } from '../../model/itemOrdem';
 
 @Component({
   selector: 'app-visualizar-entregas',
@@ -14,13 +18,28 @@ export class VisualizarEntregas {
 
   confirmado: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private ordemEntregaService: OrdemEntregaService,
+  ) { }
+
+  entregas: OrdemEntrega[] = [];
+  pedidosDaOrdem: ItemOrdem[] = [];
 
   @ViewChild('myModal') modal!: ElementRef;
   @ViewChild('myInput') input!: ElementRef;
 
-  enviarPara(rota: string) {
-    this.router.navigate([rota]);
+  enviarPara(rota: string, id?: number) {
+    if (id) {
+      this.router.navigate([rota], { queryParams: { id } });
+    }
+    else {
+      this.router.navigate([rota]);
+    }
+  }
+
+  ngOnInit() {
+    this.getEntregas();
   }
 
   ngAfterViewInit() {
@@ -33,5 +52,22 @@ export class VisualizarEntregas {
 
   }
 
+  getEntregas(termobusca?: string) {
+    this.ordemEntregaService.get(termobusca).subscribe({
+      next: (registro: OrdemEntrega[]) => {
+        this.entregas = registro;
+      }
+    });
+  }
+
+  getItensOrdem(id: number) {
+
+    this.ordemEntregaService.getPedidos(id).subscribe({
+      next: (registro: ItemOrdem[]) => {
+        this.pedidosDaOrdem = registro;
+      }
+    })
+
+  }
 
 }
