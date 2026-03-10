44f1f04464bec0d585c1a35ced49b04a5dd169df
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-03-10 16:47:24 -0500

    feat: adicinonando o usuario na tela de login

--- sige-api/usuario/admin.py
+++ sige-api/usuario/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from usuario.models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informacoes adicionais', {'fields': ('papel',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informacoes adicionais', {'fields': ('papel',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'papel', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
