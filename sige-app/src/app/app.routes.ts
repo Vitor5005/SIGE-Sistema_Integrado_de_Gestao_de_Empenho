9c4576a0aaceb1e571002b845e2882bba9bd1f47
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2026-03-13 11:39:15 -0500

    remove: removendo o caminho ate a pagina de cadastro

--- sige-app/src/app/app.routes.ts
+++ sige-app/src/app/app.routes.ts
@@ -32,10 +32,6 @@ export const routes: Routes = [
     component: RecuperarSenha
   },
   {
-    path: 'cadastrar-se',
-    component: Cadastro
-  },
-  {
     path: "",
     canActivate: [authGuard],
     children: [
